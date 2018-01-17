5af635cb6eedc6e908d156d5cb1aae4bb3f55409
Hang Yang <yangdehang0204@gmail.com>
2018-01-16 22:25:51 -0500

    add rendering loop

--- flaskr/app/routes.py
+++ flaskr/app/routes.py
@@ -8,4 +8,15 @@ from app import app #app is the member of app package
 @app.route('/index')
 def index():
 	user = {'username':'Hang'}
-	return render_template('index.html', user = user)
+	posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+
+	return render_template('index.html',title = 'Home', user = user,posts = posts)
